b30f81cd1103e198dd96d6a48b4702a5675f0585
Buddster124 <Buddster3@protonmail.com>
2020-10-09 00:25:27 -0500

    Update RemoveWindowsUpdate.ps1

--- RemoveWindowsUpdate.ps1
+++ RemoveWindowsUpdate.ps1
@@ -11,11 +11,42 @@ function Show-Menu
         [string]$Title = 'Windows_Update'
     )
     Clear-Host
-    Write-Host "================ $Title ================"
+    Write-Host "Script Created By: Anthony Budd"
+    Write-Host "Last Edit: 10/09/2020"
+    Write-Host "================================================================================"
+    Write-Host "   __  __          __      __          ____                                     "
+    Write-Host "  / / / /___  ____/ /___ _/ /____     / __ \___  ____ ___  ____ _   _____  _____"
+    Write-Host " / / / / __ \/ __  / __ `/ __/ _ \   / /_/ / _ \/ __ `__ \/ __ \ | / / _ \/ ___/"
+    Write-Host "/ /_/ / /_/ / /_/ / /_/ / /_/  __/  / _, _/  __/ / / / / / /_/ / |/ /  __/ /    "
+    Write-Host "\____/ .___/\__,_/\__,_/\__/\___/  /_/ |_|\___/_/ /_/ /_/\____/|___/\___/_/     "
+    Write-Host "    /_/                                                                         "
+    Write-Host "================================================================================"
     
     Write-Host "1: Press '1' To Disable Windows Update. (**Recomended**)"
     Write-Host "2: Press '2' To Renable Windows Update. (**Will Not Work If You Destroyed Windows Update**)"
     Write-Host "3: Press '3' To Destroy Windows Update. (**Destructive**)"
+    Write-Host "Q: Press 'Q' to quit."
+}
+
+#Menu 2 (Verify Removal Of Windows Update)
+function Show-Menu-Remove-Update
+{
+    param (
+        [string]$Title = 'Confirm Choice'
+    )
+    Clear-Host
+    Write-Host "==================== $Title ====================" -ForegroundColor Red
+    Write-Host "    #     #                                      " -ForegroundColor Red
+    Write-Host "    #  #  #   ##   #####  #    # # #    #  ####  " -ForegroundColor Red
+    Write-Host "    #  #  #  #  #  #    # ##   # # ##   # #    # " -ForegroundColor Red
+    Write-Host "    #  #  # #    # #    # # #  # # # #  # #      " -ForegroundColor Red
+    Write-Host "    #  #  # ###### #####  #  # # # #  # # #  ### " -ForegroundColor Red
+    Write-Host "    #  #  # #    # #   #  #   ## # #   ## #    # " -ForegroundColor Red
+    Write-Host "     ## ##  #    # #    # #    # # #    #  ####  " -ForegroundColor Red
+    Write-Host "==================== $Title ====================" -ForegroundColor Red
+
+    Write-Host "This action CAN NOT be undone!!!" -ForegroundColor Red -BackgroundColor Yellow
+    Write-Host "CONTINUE: Type 'CONTINUE' to continue."
     Write-Host "Q: Press 'Q' to quit."
 }
 
@@ -94,7 +125,7 @@ function Restore-Update {
 do
  {
      Show-Menu
-     $selection = Read-Host "Please make a selection"
+     $selection = Read-Host ">"
      switch ($selection)
      {
          '1' {
@@ -104,10 +135,22 @@ do
              'Restoring Windows Update'
             Restore-Update
          } '3' {
-             'Removing Windows Update'
-            Remove-Update
+             do #confirm choice to remove Windows Update
+             {
+                Show-Menu-Remove-Update
+                 $selection2 = Read-Host ">"
+                 switch ($selection2)
+                 {
+                     'CONTINUE' {
+                         'Removing Windows Update...'
+                         Remove-Update
+                         Pause
+                         break
+                     }  
+                 }
+             }
+             until ($selection2 -eq 'q')
          }
      }
-     pause
  }
  until ($selection -eq 'q')
